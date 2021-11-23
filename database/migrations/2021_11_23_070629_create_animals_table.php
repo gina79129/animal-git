7a00901215134cc1c6a733166ce53e56ce46d970
gina79129 <lovemethanks@gmail.com>
2021-11-23 16:54:28 +0800

    create animal database and add api resource to controller/AnimalController

--- database/migrations/2021_11_23_070629_create_animals_table.php
+++ database/migrations/2021_11_23_070629_create_animals_table.php
@@ -14,7 +14,16 @@ class CreateAnimalsTable extends Migration
     public function up()
     {
         Schema::create('animals', function (Blueprint $table) {
-            $table->id();
+            $table->id()->comment('ID');
+            //unsignedBigInteger 無符號的整數類型，只存正整數，可以存取比較大範圍的數字
+            $table->unsignedBigInteger('type_id')->nullable()->comment('動物分類');
+            $table->string('name')->comment("動物暱稱");
+            $table->date('birthday')->nullable()->comment("生日");
+            $table->string('area')->nullable()->comment("所在地區");
+            $table->boolean('fix')->default(false)->comment("結紮情形");
+            $table->text('description')->nullable()->comment("簡單敘述");
+            $table->text('personality')->nullable()->comment("動物個性");
+            $table->unsignedBigInteger('user_id')->comment("所屬會員");
             $table->timestamps();
         });
     }
