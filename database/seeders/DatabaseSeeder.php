f164af6b687ca910bfd1c966acd7a1e4ad6afd7a
gina79129 <lovemethanks@gmail.com>
2021-11-24 17:16:52 +0800

    add factory && seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Animal;
+use App\Models\User;
+use Illuminate\Support\Facades\Schema;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +17,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        //取消外鍵約束
+        Schema::disableForeignKeyConstraints();
+        Animal::truncate(); //清空animal資料表 ID歸零
+        User::truncate(); //清空users資料表 ID歸零
+
+        //建立5筆會員測試資料
+        User::factory(5)->create();
+        //建立一萬筆動物測試資料
+        Animal::factory(10000)->create();
+        //開啟外鍵約束
+        Schema::enableForeignKeyConstraints();
     }
 }
