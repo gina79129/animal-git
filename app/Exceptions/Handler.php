7a302643755b22b7835ed733b13b0208c373eef6
gina79129 <lovemethanks@gmail.com>
2021-11-24 10:49:03 +0800

    add exception rander

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,8 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Symfony\Component\HttpFoundation\Response;
 
 class Handler extends ExceptionHandler
 {
@@ -37,5 +39,27 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    /**
+     * render 方法分別傳入$request 使用者請求資料以及發生例外的$exception 資料
+     */
+    public function render($request, Throwable $exception){
+        // dd($exception);
+        if($request->expectsJson()){
+        //如果使用者請求伺服器回傳JSON格式，就執行以下程式  (在使用postman時 加入Accept:application/json，就是在跟API伺服器說，請回傳JSON格式給我)
+        //instanceof(型態運算子) 檢查$exception這個被攔截到的例外是不是ModelNotFoundException類別
+            if($exception instanceof ModelNotFoundException){
+                //攔截到例外，做出對應的處理，回傳404 Not Found 狀態碼，並附上錯誤資訊優化操作API上的體驗
+                return response()->json([
+                    'code' => '404',
+                    'message' => '找不到資源',
+                    'description' => '此資料已經刪除或不存在'
+                    ],
+                    Response::HTTP_NOT_FOUND
+                );
+            }
+        }
+        return parent::render($request,$exception);
     }
 }
