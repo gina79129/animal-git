d828e8195419efe68ca293d80cb227ae41a59875
gina79129 <lovemethanks@gmail.com>
2021-11-26 11:18:38 +0800

    add cache

--- app/Http/Controllers/AnimalController.php
+++ app/Http/Controllers/AnimalController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Animal;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Cache;
 
 class AnimalController extends Controller
 {
@@ -15,8 +16,44 @@ class AnimalController extends Controller
      */
     public function index(Request $request)
     {
+        //參考網址:https://laravel.com/docs/8.x/queries
+        //以下Model查詢建構器的程式碼
+        // $data = Animal::where('area','高雄')->where('name','like','%小%')->get();
+        //等於在資料庫中執行下方sql語法
+        //select * from `animals` where area='高雄' and name like '%小%';
+
+        //使用集合類別Illuminate\Support\Collection  pluck可以把集合中的key取出
+        // $collection = collect([
+        //     ['id'=>'1','name'=>'伍怡'],
+        //     ['id'=>'2','name'=>'文庭淑']
+        // ]);
+        
+        // $plucked = $collection->pluck('name');
+        // $plucked->all();
+
+        //從資料庫讀取資料之後，想如何整理資料可以參考Laravel集合官方文件，各種方法及範例都有
+        //https://laravel.com/docs/8.x/collections#the-enumerable-contract
+
         //設定預設值
         $limit = $request->limit ?? 10; //未設定預設值為10
+
+        //使用網址設定為快取檔案名稱
+        //取得網址
+        $url = $request->url();
+        //取得query的參數， 例如:?limit=5&page=2 網址問號後面的參數
+        $queryParams = $request->query();
+        //每個人請求的query參數順序可能不同，使用參數第一個英文字排序
+        ksort($queryParams);
+        //利用http_build_query方法將查詢參數轉為字串
+        $queryString = http_build_query($queryParams);
+        //組合成完整網址
+        $fullUrl = "{$url}?{$queryString}";
+
+        //使用Laravel的快取方法檢查是否有快取紀錄
+        if(Cache::has($fullUrl)){
+            //使用return直接回傳快取資料，不做其他程式邏輯
+            return Cache::get($fullUrl);
+        }
 
         //建立查詢建構器，分段的方式撰寫SQL語句
         $query = Animal::query();
@@ -44,13 +81,16 @@ class AnimalController extends Controller
         }
 
         //使用Model orderBy方法加入SQL語法排序條件，依照 id 由大到小排序
-        $animals = $query->orderBy('id','desc')
-                         ->paginate($limit) //使用分頁方法，最多回傳$limit筆資料
+        $animals = $query->paginate($limit) //使用分頁方法，最多回傳$limit筆資料
                          ->appends($request->query()); //appends主要是可以將使用者請求的參數附加在分頁資訊中，
                                                          //如first_page_url網址後會包含limit參數，表示使用者請
                                                          //求時，設定?limit=10限制，回傳分頁訊息時自動加上limit
                                                          //參數方便客戶端下次再執行請求，不會忘記加篩選規則 
-        return response(['data'=>$animals,'message'=>''],Response::HTTP_OK);
+        
+        //沒有快取記錄記住資料，並設定60秒過期，快取名稱使用網址命名
+        return Cache::remember($fullUrl,60,function() use ($animals){
+            return response(['data'=>$animals,'message'=>''],Response::HTTP_OK);
+        });
     }
 
     /**
