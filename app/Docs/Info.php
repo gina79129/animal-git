aab994f381db22abce16904af530f05fb31c8781
gina79129 <lovemethanks@gmail.com>
2021-12-06 17:45:43 +0800

    add swagger

---
+++ app/Docs/Info.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="送養系統API",
+ *      description=" Victor RESTful API ",
+ *      @OA\Contact(
+ *          email="lovemethanks@gmail.com"
+ *      )
+ * )
+ */
+?>
